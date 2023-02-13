cd516ae98d8b40d9f43b6ef072bdde1487f40dea
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-13 22:46:40 +0530

    loaded adb

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="w-full h-full grid justify-center bg-gray-900">
       <Text className="text-violet-500 text-2xl font-bold text-center">
-        NativeWind + <Text className="text-white">Expo</Text>
+        NativeWind + <Text className="text-white">ADB</Text>
       </Text>
       <StatusBar style="auto" />
     </View>
