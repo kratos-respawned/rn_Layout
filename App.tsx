aaf0ab8f4997f2f371ca5190caae5ba8930fc9c4
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-26 10:25:55 +0530

    created basic layout

--- App.tsx
+++ App.tsx
@@ -3,18 +3,22 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="  w-full h-full bg-purple-700"  >
-      <View className="bg-red-500 flex-1">
-        <Text>
-          Flex-1
-        </Text>
+    <View className="  w-full h-full "  >
+      <StatusBar style="auto" />
+      <View className="bg-fuchsia-500 flex-row  flex-[2]">
+        <View className="bg-red-500 flex-1 m-3">
+        </View>
+        <View className="bg-teal-500 flex-1 m-3">
+        </View>
+        <View className="bg-rose-500 flex-1 m-3">
+        </View>
       </View>
-      <View className="bg-green-500 flex-1">
+      <View className="bg-green-500 flex-1 ">
         <Text>
           Flex-2
         </Text>
       </View>
-      <View className="bg-blue-500 flex-1">
+      <View className="bg-blue-500 flex-1 ">
         <Text>
           Flex-3
         </Text>
