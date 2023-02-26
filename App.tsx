3d1ddb6a4e5ecd1f73f111ed3bc663ec24c6e30f
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-02-26 10:17:35 +0530

    flex-1

--- App.tsx
+++ App.tsx
@@ -3,11 +3,22 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="w-full h-full grid justify-center bg-gray-900">
-      <Text className="text-violet-500 text-2xl font-bold text-center">
-        NativeWind + <Text className="text-white">ADB</Text>
-      </Text>
-      <StatusBar style="auto" />
+    <View className="  w-full h-full bg-purple-700"  >
+      <View className="bg-red-500 flex-1">
+        <Text>
+          Flex-1
+        </Text>
+      </View>
+      <View className="bg-green-500 flex-1">
+        <Text>
+          Flex-2
+        </Text>
+      </View>
+      <View className="bg-blue-500 flex-1">
+        <Text>
+          Flex-3
+        </Text>
+      </View>
     </View>
   );
 }
